10cf1bf755a09aaa5e64eaf4a55d6fee58bf149b
iuki <siliconvall3y@gmail.com>
2025-03-19 12:48:19 +0200

    date filter changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import {provideRouter, withComponentInputBinding} from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import {provideHttpClient, withFetch} from '@angular/common/http';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
--- src/app/components/dashboard/partials/date-filter/date-filter.component.ts
+++ src/app/components/dashboard/partials/date-filter/date-filter.component.ts
@@ -3,15 +3,29 @@ import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MatIconModule} from '@angular/material/icon';
+import {provideMomentDateAdapter} from '@angular/material-moment-adapter';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'MMMM DD, YYYY',
+  },
+  display: {
+    dateInput: 'MMMM DD, YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'MMMM DD, YYYY',
+    monthYearA11yLabel: 'MMMM YYYY',
+  },
+};
 
 @Component({
   selector: 'app-date-filter',
   imports: [MatFormFieldModule, MatDatepickerModule, MatIconModule],
   templateUrl: './date-filter.component.html',
   styleUrl: './date-filter.component.scss',
-  providers: [provideNativeDateAdapter()],
+  providers: [provideMomentDateAdapter(MY_DATE_FORMATS),],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class DateFilterComponent {
 
 }
